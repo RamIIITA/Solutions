7aee06531e699b10498953da0001feadb3544270
RamIIITA <46890642+RamIIITA@users.noreply.github.com>
2019-12-02 15:25:40 +0530

    solution for factorial problem
    Above mentioned code is a solution for a finding the reminders of the factors for  a given problem
    Example: 
    For a Given 10 
    solution: 1 2 0 0
    i.e 1*3! = 6
         2*2! = 4
         0 *1! = 0
          0*0! = 0
    
    where the multiplers of the factorail(n) must less than equal to n
    factorial     multipliers 
    0!   * {0}
    1!    *{0,1}
    2!  *{0,1,2}
    3! *{0,1,2,3}
    
    
    another example:
    Given 23
    Solution: 3 2 1 0
    3*3! = 18
    2*2! = 4
    1*1! = 1
    sum = 18+4+1 = 23 (or)
               3*(3!)+2(2!)+1(1!)+0(0!) = 23

---
+++ reminders of the factorial.py
@@ -0,0 +1,52 @@
+#Fac Function results facotrial for a particular number
+def fact(n):
+    Sum = 1
+    if n<3:
+        return n
+    for i in range(2,n+1):
+        Sum*=i
+    return Sum
+
+#solution part
+def sol(n):
+    temp = 1
+    fac = [0,1]
+    dic_fac = {1:1,2:2,0:0}
+    for i in range(2,n):
+        factor = fact(i)
+        temp+=(factor*i)
+        dic_fac[i] = factor
+        fac.append(i)
+        if n<temp:
+            break
+    
+    ans=dict()
+    for i in fac:
+        ans[i]=0
+    
+    while(n):
+        for i in reversed(fac):
+            for j in range(i,-1,-1):
+                if (j*dic_fac[i]) <=  n:
+                    n-=(j*dic_fac[i])
+                    ans[i] = j
+                    break
+                if n<1:
+                    break
+                
+            if n<1:
+                break
+    result = ""
+    for i in sorted(ans.keys(), reverse = True):
+        result=result+str(ans[i])+" "
+    return result
+    
+#checking the base conndition    
+def fun(n):
+    if n<1:
+        return 0
+    if n<2:
+        return str(1)+" "+str(0)
+    return sol(n)
+    
+print(fun(21))
